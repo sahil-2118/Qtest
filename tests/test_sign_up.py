701d75b096293571348ae3205bc53a05abd0fc95
root <root@ubuntu.myguest.virtualbox.org>
2023-10-21 05:28:38 -0700

    fix bugs of test_sign_up

--- tests/test_sign_up.py
+++ tests/test_sign_up.py
@@ -3,35 +3,35 @@ import subprocess
 import os
 import signal
 import time
-import requests
-import json
+
+__TEST_FILE_BASE_ADDRESS = r"commands/sign-up/"
 
 
 @pytest.fixture(scope="function")
-def server():
-    # Start the server as a background process
-    args = ["python3", "server.py", "--port", "7878", "--ip", "127.0.0.1"]
+def set_up():
+    args = [
+            "python3", 
+            "server.py", 
+            "--port", 
+            "7878", 
+            "--ip", 
+            "127.0.0.1"
+            ]
     server_process = subprocess.Popen(args)
-    # Get the process ID
     pid = server_process.pid
-    # Wait for the server to be ready
     time.sleep(2)
-    # Yield the process ID
     yield pid
-    # Terminate the server process
     os.kill(pid, signal.SIGTERM)
 
 
 
-def test_sign_up(server):
-    # Use the fixture to start and stop the server
-    # Send a sign up request to the server with valid data
+def sign_up_functiona(cammand_address : str)-> str:
     args = ["python3", 
             "client.py", 
             "--port", "7878", 
             "--ip", "127.0.0.1", 
             "--file", 
-            "commands/sign-up.json",
+            cammand_address ,
             ]
     client_process = subprocess.run(
                                 args,
@@ -42,7 +42,15 @@ def test_sign_up(server):
     time.sleep(2)
    
     assert client_process.returncode == 0
-    assert "Response is" in client_process.stdout
-    assert client_process.stderr == ""    
-    
+    assert client_process.stderr == ""  
+
+    return client_process.stdout
+
+
+def test_sign_up(set_up):
+      with open(__TEST_FILE_BASE_ADDRESS + 'commands_file_names') as f:
+           lines = f.readlines()
+           for commands_address in lines:
+                out_message = sign_up_functiona(__TEST_FILE_BASE_ADDRESS + commands_address)
+                assert 'successfully' in out_message
     
